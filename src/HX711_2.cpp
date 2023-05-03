9039549642e0a405a4c4167b99156e90665178de
banoz <banoz@users.noreply.github.com>
2023-05-03 01:21:07 -0500

    feature/timer starts here

--- src/HX711_2.cpp
+++ src/HX711_2.cpp
@@ -5,315 +5,236 @@
  *
  * MIT License
  * (c) 2018 Bogdan Necula
+ * (c) 2023 Vas
  *
 **/
-#include <Arduino.h>
+
 #include "HX711_2.h"
 
-// TEENSYDUINO has a port of Dean Camera's ATOMIC_BLOCK macros for AVR to ARM Cortex M3.
-#define HAS_ATOMIC_BLOCK (defined(ARDUINO_ARCH_AVR) || defined(TEENSYDUINO))
+HX711* _theHX711;
 
-// Whether we are running on either the ESP8266 or the ESP32.
-#define ARCH_ESPRESSIF (defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32))
+HX711::HX711(TIM_TypeDef* timerInstance) {
+  _theHX711 = this;
 
-// Whether we are actually running on FreeRTOS.
-#define IS_FREE_RTOS defined(ARDUINO_ARCH_ESP32)
+  _hx711ReadTimer = new HardwareTimer(timerInstance);
+  _hx711ReadTimer->setOverflow(2, MICROSEC_FORMAT);
+  _hx711ReadTimer->attachInterrupt(_onHX711ReadTimerInterrupt);
+}
 
-// Define macro designating whether we're running on a reasonable
-// fast CPU and so should slow down sampling from GPIO.
-#define FAST_CPU \
-    ( \
-    ARCH_ESPRESSIF || \
-    defined(ARDUINO_ARCH_SAM)     || defined(ARDUINO_ARCH_SAMD) || \
-    defined(ARDUINO_ARCH_STM32)   || defined(TEENSYDUINO) || \
-    defined(ARDUINO_ARCH_RP2040) \
-    )
+HX711::~HX711() {
+}
 
-#define FAST_CPU_DELAY_US 3
+void HX711::begin(uint32_t dout, uint32_t dout2, uint32_t pd_sck, uint8_t gain) {
+  PD_SCK = pd_sck;
+  DOUT = dout;
+  DOUT2 = dout2;
 
-#if HAS_ATOMIC_BLOCK
-// Acquire AVR-specific ATOMIC_BLOCK(ATOMIC_RESTORESTATE) macro.
-#include <util/atomic.h>
-#endif
+  PD_SCK_PN = digitalPinToPinName(PD_SCK);
+  DOUT_PN = digitalPinToPinName(DOUT);
+  DOUT2_PN = digitalPinToPinName(DOUT2);
 
-uint16_t shiftInSlow(uint8_t dataPin, uint8_t dataPin2, uint8_t clockPin, uint8_t bitOrder) {
-    uint8_t value = 0;
-    uint8_t value2 = 0;
-    uint8_t i;
-    
-    for(i = 0; i < 8; ++i) {
-        digitalWrite(clockPin, HIGH);
-        delayMicroseconds(FAST_CPU_DELAY_US);
-        digitalWrite(clockPin, LOW);
-        delayMicroseconds(FAST_CPU_DELAY_US);
-        if(bitOrder == LSBFIRST) {
-            value |= digitalRead(dataPin) << i;
-            value2 |= digitalRead(dataPin2) << i;
-        } else {
-            value |= digitalRead(dataPin) << (7 - i);
-            value2 |= digitalRead(dataPin2) << (7 - i);
+  pinMode(PD_SCK, OUTPUT);
+  pinMode(DOUT, INPUT);
+  pinMode(DOUT2, INPUT);
+
+  set_gain(gain);
+}
+
+bool HX711::is_ready(void) {
+  return readStatus == 0u;
+}
+
+void HX711::set_gain(uint8_t gain) {
+  switch (gain) {
+  case 128u:		// channel A, gain factor 128
+    GAIN = 1u;
+    break;
+  case 64u:		// channel A, gain factor 64
+    GAIN = 3u;
+    break;
+  case 32u:		// channel B, gain factor 32
+    GAIN = 2u;
+    break;
+  }
+}
+
+void HX711::read(long* readValues, unsigned long timeout) {
+
+  // Wait for the chip to become ready.
+  if (!wait_ready_timeout(timeout)) {
+    return;
+  }
+
+  readValues[0] = static_cast<long>(readData[0]) - OFFSET;
+  readValues[1] = static_cast<long>(readData[1]) - OFFSET2;
+}
+
+void HX711::wait_ready(unsigned long delay_ms) {
+  // Wait for the chip to become ready.
+  // This is a blocking implementation and will
+  // halt the sketch until a load cell is connected.
+  while (!is_ready()) {
+    // Probably will do no harm on AVR but will feed the Watchdog Timer (WDT) on ESP.
+    // https://github.com/bogde/HX711/issues/73
+    delay(delay_ms);
+  }
+}
+
+bool HX711::wait_ready_retry(unsigned int retries, unsigned long delay_ms) {
+  // Wait for the chip to become ready by
+  // retrying for a specified amount of attempts.
+  // https://github.com/bogde/HX711/issues/76
+  int count = 0;
+  while (count < retries) {
+    if (is_ready()) {
+      return true;
+    }
+    delay(delay_ms);
+    count++;
+  }
+  return false;
+}
+
+bool HX711::wait_ready_timeout(unsigned long timeout, unsigned long delay_ms) {
+  // Wait for the chip to become ready until timeout.
+  // https://github.com/bogde/HX711/pull/96
+  unsigned long stopAt = millis() + timeout;
+  while (millis() < stopAt) {
+    if (is_ready()) {
+      return true;
+    }
+    delay(delay_ms);
+  }
+  return false;
+}
+
+void HX711::read_average(long* readValues, byte times) {
+  long sum[2] = { 0 };
+  for (byte i = 0; i < times; ++i) {
+    long values[2];
+    read(values);
+    sum[0] += values[0];
+    sum[1] += values[1];
+  }
+  readValues[0] = sum[0] / times;
+  readValues[1] = sum[1] / times;
+}
+
+void HX711::get_value(long* readValues, byte times) {
+  read_average(readValues, times);
+}
+
+void HX711::get_units(float* readValues, byte times) {
+  long values[2];
+  read_average(values, times);
+
+  readValues[0] = values[0] / (SCALE == 0 ? 1 : SCALE);
+  readValues[1] = values[1] / (SCALE2 == 0 ? 1 : SCALE2);
+}
+
+void HX711::tare(byte times) {
+  long readValues[2];
+  set_offset(0, 0);
+  read_average(readValues, times);
+  set_offset(readValues[0], readValues[1]);
+}
+
+void HX711::set_scale(float scale, float scale2) {
+  SCALE = scale;
+  SCALE2 = scale2;
+}
+
+void HX711::get_scale(float* scaleValues) {
+  scaleValues[0] = SCALE;
+  scaleValues[1] = SCALE2;
+}
+
+void HX711::set_offset(long offset, long offset2) {
+  OFFSET = offset;
+  OFFSET2 = offset2;
+}
+
+void HX711::get_offset(long* offsetValues) {
+  offsetValues[0] = OFFSET;
+  offsetValues[1] = OFFSET2;
+}
+
+void HX711::power_down() {
+  _hx711ReadTimer->pause();
+  readStatus = 0;
+  digitalWrite(PD_SCK, HIGH);
+}
+
+void HX711::power_up() {
+  digitalWrite(PD_SCK, LOW);
+  readStatus = 0;
+  _hx711ReadTimer->resume();
+}
+
+void HX711::processReadTimerInterrupt(void) {
+  if (readStatus == 0u) {
+    if (digitalReadFast(DOUT_PN) || digitalReadFast(DOUT2_PN)) { // conversion not ready
+      return;
+    }
+  }
+
+  uint8_t position = readStatus >> 1;
+
+  if (readStatus & 1u) {
+    if (position < 24u) { // 24 data bits
+      if (position == 0u) {
+        readBuffer[0] = 0u;
+        readBuffer[1] = 0u;
+      }
+
+      if (digitalReadFast(DOUT_PN)) {
+        readBuffer[0] |= (1u << (23u - position));
+      }
+
+      if (digitalReadFast(DOUT2_PN)) {
+        readBuffer[1] |= (1u << (23u - position));
+      }
+
+      if (position == 23u) { // all bits received
+        // Replicate the most significant bit to pad out a 32-bit signed integer
+        if (readBuffer[0] & 0x800000) {
+          readData[0] = readBuffer[0] | 0xFF000000;
         }
+        else {
+          readData[0] = readBuffer[0];
+        }
+        if (readBuffer[1] & 0x800000 > 0) {
+          readData[1] = readBuffer[1] | 0xFF000000;
+        }
+        else {
+          readData[1] = readBuffer[1];
+        }
+      }
     }
-    
-    return value2 << 8 | value;
-}
-
-
-#if ARCH_ESPRESSIF
-// ESP8266 doesn't read values between 0x20000 and 0x30000 when DOUT is pulled up.
-#define DOUT_MODE INPUT
-#else
-#define DOUT_MODE INPUT_PULLUP
-#endif
-
-
-HX711_2::HX711_2() {
-}
-
-HX711_2::~HX711_2() {
-}
-
-void HX711_2::begin(byte dout, byte dout2, byte pd_sck, byte gain) {
-	PD_SCK = pd_sck;
-	DOUT = dout;
-	DOUT2 = dout2;
-
-	pinMode(PD_SCK, OUTPUT);
-	pinMode(DOUT, DOUT_MODE);
-	pinMode(DOUT2, DOUT_MODE);
-
-	set_gain(gain);
-}
-
-bool HX711_2::is_ready() {
-    if (digitalRead(DOUT2) == HIGH) {
-		return false;
+    digitalWriteFast(PD_SCK_PN, LOW);
+  }
+  else {
+    if (position < 24u) { // 24 data bits
+      digitalWriteFast(PD_SCK_PN, HIGH);
     }
-
-	return digitalRead(DOUT) == LOW;
-}
-
-void HX711_2::set_gain(byte gain) {
-	switch (gain) {
-		case 128:		// channel A, gain factor 128
-			GAIN = 1;
-			break;
-		case 64:		// channel A, gain factor 64
-			GAIN = 3;
-			break;
-		case 32:		// channel B, gain factor 32
-			GAIN = 2;
-			break;
-	}
-
-}
-
-void HX711_2::read(long* readValues, unsigned long timeout) {
-
-	// Wait for the chip to become ready.
-	if (!wait_ready_timeout(timeout)) {
-        return;
+    // set gain bits
+    else if (position == 24u && GAIN > 0) {
+      digitalWriteFast(PD_SCK_PN, HIGH);
     }
+    else if (position == 25u && GAIN > 1) {
+      digitalWriteFast(PD_SCK_PN, HIGH);
+    }
+    else if (position == 26u && GAIN > 2) {
+      digitalWriteFast(PD_SCK_PN, HIGH);
+    }
+  }
 
-	// Define structures for reading data into.
-	unsigned long value = 0;
-	uint8_t data[6] = { 0 };
-	uint8_t filler = 0x00;
-
-	// Protect the read sequence from system interrupts.  If an interrupt occurs during
-	// the time the PD_SCK signal is high it will stretch the length of the clock pulse.
-	// If the total pulse time exceeds 60 uSec this will cause the HX711 to enter
-	// power down mode during the middle of the read sequence.  While the device will
-	// wake up when PD_SCK goes low again, the reset starts a new conversion cycle which
-	// forces DOUT high until that cycle is completed.
-	//
-	// The result is that all subsequent bits read by shiftIn() will read back as 1,
-	// corrupting the value returned by read().  The ATOMIC_BLOCK macro disables
-	// interrupts during the sequence and then restores the interrupt mask to its previous
-	// state after the sequence completes, insuring that the entire read-and-gain-set
-	// sequence is not interrupted.  The macro has a few minor advantages over bracketing
-	// the sequence between `noInterrupts()` and `interrupts()` calls.
-	#if HAS_ATOMIC_BLOCK
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-
-	#elif IS_FREE_RTOS
-	// Begin of critical section.
-	// Critical sections are used as a valid protection method
-	// against simultaneous access in vanilla FreeRTOS.
-	// Disable the scheduler and call portDISABLE_INTERRUPTS. This prevents
-	// context switches and servicing of ISRs during a critical section.
-	portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-	portENTER_CRITICAL(&mux);
-
-	#else
-	// Disable interrupts.
-	noInterrupts();
-	#endif
-
-	// Pulse the clock pin 24 times to read the data.
-	uint16_t readBits = shiftInSlow(DOUT, DOUT2, PD_SCK, MSBFIRST);
-    data[2] = readBits & 0xFF;
-    data[5] = readBits >> 8;
-    readBits = shiftInSlow(DOUT, DOUT2, PD_SCK, MSBFIRST);
-    data[1] = readBits & 0xFF;
-    data[4] = readBits >> 8;
-    readBits = shiftInSlow(DOUT, DOUT2, PD_SCK, MSBFIRST);
-    data[0] = readBits & 0xFF;
-    data[3] = readBits >> 8;
-
-	// Set the channel and the gain factor for the next reading using the clock pin.
-	for (unsigned int i = 0; i < GAIN; i++) {
-		digitalWrite(PD_SCK, HIGH);
-		#if FAST_CPU
-		delayMicroseconds(FAST_CPU_DELAY_US);
-		#endif
-		digitalWrite(PD_SCK, LOW);
-		#if FAST_CPU
-		delayMicroseconds(FAST_CPU_DELAY_US);
-		#endif
-	}
-
-	#if IS_FREE_RTOS
-	// End of critical section.
-	portEXIT_CRITICAL(&mux);
-
-	#elif HAS_ATOMIC_BLOCK
-	}
-
-	#else
-	// Enable interrupts again.
-	interrupts();
-	#endif
-
-	// Replicate the most significant bit to pad out a 32-bit signed integer
-	if (data[2] & 0x80) {
-		filler = 0xFF;
-	} else {
-		filler = 0x00;
-	}
-
-	// Construct a 32-bit signed integer
-	value = ( static_cast<unsigned long>(filler) << 24
-            | static_cast<unsigned long>(data[2]) << 16
-            | static_cast<unsigned long>(data[1]) << 8
-            | static_cast<unsigned long>(data[0]) );
-        
-    readValues[0] = static_cast<long>(value) - OFFSET;
-    
-    if (data[5] & 0x80) {
-        filler = 0xFF;
-    } else {
-        filler = 0x00;
-    } 
-    
-    // Construct a 32-bit signed integer
-    value = ( static_cast<unsigned long>(filler) << 24
-            | static_cast<unsigned long>(data[5]) << 16
-            | static_cast<unsigned long>(data[4]) << 8
-            | static_cast<unsigned long>(data[3]) );
-    
-    readValues[1] = static_cast<long>(value) - OFFSET2;
-
+  if (++readStatus > 63u) { // wrap around
+    digitalWriteFast(PD_SCK_PN, LOW);
+    readStatus = 0;
+  }
 }
 
-void HX711_2::wait_ready(unsigned long delay_ms) {
-	// Wait for the chip to become ready.
-	// This is a blocking implementation and will
-	// halt the sketch until a load cell is connected.
-	while (!is_ready()) {
-		// Probably will do no harm on AVR but will feed the Watchdog Timer (WDT) on ESP.
-		// https://github.com/bogde/HX711/issues/73
-		delay(delay_ms);
-	}
-}
-
-bool HX711_2::wait_ready_retry(int retries, unsigned long delay_ms) {
-	// Wait for the chip to become ready by
-	// retrying for a specified amount of attempts.
-	// https://github.com/bogde/HX711/issues/76
-	int count = 0;
-	while (count < retries) {
-		if (is_ready()) {
-			return true;
-		}
-		delay(delay_ms);
-		count++;
-	}
-	return false;
-}
-
-bool HX711_2::wait_ready_timeout(unsigned long timeout, unsigned long delay_ms) {
-	// Wait for the chip to become ready until timeout.
-	// https://github.com/bogde/HX711/pull/96
-	unsigned long stopAt = millis() + timeout;
-	while (millis() < stopAt) {
-		if (is_ready()) {
-			return true;
-		}
-		delay(delay_ms);
-	}
-	return false;
-}
-
-void HX711_2::read_average(long* readValues, byte times) {
-	long sum[2] = { 0 };
-	for (byte i = 0; i < times; ++i) {
-		long values[2];
-        read(values);
-        sum[0] += values[0];
-        sum[1] += values[1];
-	}
-	readValues[0] = sum[0] / times;
-	readValues[1] = sum[1] / times;
-}
-
-void HX711_2::get_value(long* readValues, byte times) {
-	read_average(readValues, times);
-}
-
-void HX711_2::get_units(float* readValues, byte times) {
-	long values[2];
-    read_average(values, times);
-    
-    readValues[0] = values[0] / (SCALE == 0 ? 1 : SCALE);
-    readValues[1] = values[1] / (SCALE2 == 0 ? 1 : SCALE2);
-}
-
-void HX711_2::tare(byte times) {
-	long readValues[2];
-    set_offset(0, 0);
-    read_average(readValues, times);
-    set_offset(readValues[0], readValues[1]);
-}
-
-void HX711_2::set_scale(float scale, float scale2) {
-	SCALE = scale;
-	SCALE2 = scale2;
-}
-
-void HX711_2::get_scale(float* scaleValues) {
-	scaleValues[0] = SCALE;
-    scaleValues[1] = SCALE2;
-}
-
-void HX711_2::set_offset(long offset, long offset2) {
-	OFFSET = offset;
-	OFFSET2 = offset2;
-}
-
-void HX711_2::get_offset(long* offsetValues) {
-	offsetValues[0] = OFFSET;
-    offsetValues[1] = OFFSET2;
-}
-
-void HX711_2::power_down() {
-	digitalWrite(PD_SCK, LOW);
-#if FAST_CPU
-	delayMicroseconds(FAST_CPU_DELAY_US);
-#endif	
-	digitalWrite(PD_SCK, HIGH);
-}
-
-void HX711_2::power_up() {
-	digitalWrite(PD_SCK, LOW);
+void HX711::_onHX711ReadTimerInterrupt(void) {
+  _theHX711->processReadTimerInterrupt();
 }
--- src/HX711_2.h
+++ src/HX711_2.h
@@ -7,88 +7,92 @@
  * (c) 2018 Bogdan Necula
  *
 **/
-#ifndef HX711_2_h
-#define HX711_2_h
+#pragma once
 
-#if ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
+#include <Arduino.h>
 
-class HX711_2
+class HX711
 {
-	private:
-		byte PD_SCK;	// Power Down and Serial Clock Input Pin
-		byte DOUT;		// Serial Data Output Pin
-		byte DOUT2;		// Serial Data Output Pin
-		byte GAIN;		// amplification factor
-		long OFFSET = 0;	// used for tare weight
-		long OFFSET2 = 0;	// used for tare weight
-		float SCALE = 1.f;	// used to return weight in grams, kg, ounces, whatever
-		float SCALE2 = 1.f;	// used to return weight in grams, kg, ounces, whatever
+private:
+  PinName PD_SCK_PN;
+  PinName DOUT_PN;
+  PinName DOUT2_PN;
+  uint32_t PD_SCK;	// Power Down and Serial Clock Input Pin
+  uint32_t DOUT;		// Serial Data Output Pin
+  uint32_t DOUT2;		// Serial Data Output Pin
+  uint8_t GAIN;		// amplification factor
+  long OFFSET = 0;	// used for tare weight
+  long OFFSET2 = 0;	// used for tare weight
+  float SCALE = 1.f;	// used to return weight in grams, kg, ounces, whatever
+  float SCALE2 = 1.f;	// used to return weight in grams, kg, ounces, whatever
 
-	public:
+  HardwareTimer* _hx711ReadTimer;
+  volatile uint32_t readData[2];
+  volatile uint32_t readBuffer[2];
+  volatile uint8_t readStatus;
 
-		HX711_2();
+  static inline void _onHX711ReadTimerInterrupt(void);
+  void processReadTimerInterrupt(void);
 
-		virtual ~HX711_2();
+public:
 
-		// Initialize library with data output pin, clock input pin and gain factor.
-		// Channel selection is made by passing the appropriate gain:
-		// - With a gain factor of 64 or 128, channel A is selected
-		// - With a gain factor of 32, channel B is selected
-		// The library default is "128" (Channel A).
-		void begin(byte dout, byte dout2, byte pd_sck, byte gain = 128);
+  HX711(TIM_TypeDef* timerInstance = TIM10);
 
-		// Check if HX711 is ready
-		// from the datasheet: When output data is not ready for retrieval, digital output pin DOUT is high. Serial clock
-		// input PD_SCK should be low. When DOUT goes to low, it indicates data is ready for retrieval.
-		bool is_ready();
+  virtual ~HX711();
 
-		// Wait for the HX711 to become ready
-		void wait_ready(unsigned long delay_ms = 0);
-		bool wait_ready_retry(int retries = 3, unsigned long delay_ms = 0);
-		bool wait_ready_timeout(unsigned long timeout = 1000, unsigned long delay_ms = 0);
+  // Initialize library with data output pin, clock input pin and gain factor.
+  // Channel selection is made by passing the appropriate gain:
+  // - With a gain factor of 64 or 128, channel A is selected
+  // - With a gain factor of 32, channel B is selected
+  // The library default is "128" (Channel A).
+  void begin(uint32_t dout, uint32_t dout2, uint32_t pd_sck, uint8_t gain = 128u);
 
-		// set the gain factor; takes effect only after a call to read()
-		// channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
-		// depending on the parameter, the channel is also set to either A or B
-		void set_gain(byte gain = 128);
+  // Check if HX711 is ready
+  // from the datasheet: When output data is not ready for retrieval, digital output pin DOUT is high. Serial clock
+  // input PD_SCK should be low. When DOUT goes to low, it indicates data is ready for retrieval.
+  bool is_ready(void);
 
-		// waits for the chip to be ready and returns a reading
-		void read(long* readValues, unsigned long timeout = 1000);
+  // Wait for the HX711 to become ready
+  void wait_ready(unsigned long delay_ms = 0);
+  bool wait_ready_retry(unsigned int retries = 3u, unsigned long delay_ms = 0);
+  bool wait_ready_timeout(unsigned long timeout = 1000u, unsigned long delay_ms = 0);
 
-		// returns an average reading; times = how many times to read
-		void read_average(long* readValues, byte times = 10);
+  // set the gain factor; takes effect only after a call to read()
+  // channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
+  // depending on the parameter, the channel is also set to either A or B
+  void set_gain(uint8_t gain = 128u);
 
-		// returns (read_average() - OFFSET), that is the current value without the tare weight; times = how many readings to do
-		void get_value(long* readValues, byte times = 1);
+  // waits for the chip to be ready and returns a reading
+  void read(long* readValues, unsigned long timeout = 1000u);
 
-		// returns get_value() divided by SCALE, that is the raw value divided by a value obtained via calibration
-		// times = how many readings to do  
-		void get_units(float* readValues, byte times = 1);
+  // returns an average reading; times = how many times to read
+  void read_average(long* readValues, uint8_t times = 10u);
 
-		// set the OFFSET value for tare weight; times = how many times to read the tare value
-		void tare(byte times = 10);
+  // returns (read_average() - OFFSET), that is the current value without the tare weight; times = how many readings to do
+  void get_value(long* readValues, uint8_t times = 1u);
 
-		// set the SCALE value; this value is used to convert the raw data to "human readable" data (measure units)
-		void set_scale(float scale = 1.f, float scale2 = 1.f);
+  // returns get_value() divided by SCALE, that is the raw value divided by a value obtained via calibration
+  // times = how many readings to do
+  void get_units(float* readValues, uint8_t times = 1u);
 
-		// get the current SCALE
-		void get_scale(float* scaleValues);
+  // set the OFFSET value for tare weight; times = how many times to read the tare value
+  void tare(uint8_t times = 10u);
 
-		// set OFFSET, the value that's subtracted from the actual reading (tare weight)
-		void set_offset(long offset = 0, long offset2 = 0);
+  // set the SCALE value; this value is used to convert the raw data to "human readable" data (measure units)
+  void set_scale(float scale = 1.f, float scale2 = 1.f);
 
-		// get the current OFFSET
-		void get_offset(long* offsetValues);
+  // get the current SCALE
+  void get_scale(float* scaleValues);
 
-		// puts the chip into power down mode
-		void power_down();
+  // set OFFSET, the value that's subtracted from the actual reading (tare weight)
+  void set_offset(long offset = 0, long offset2 = 0);
 
-		// wakes up the chip after power down mode
-		void power_up();
+  // get the current OFFSET
+  void get_offset(long* offsetValues);
+
+  // puts the chip into power down mode
+  void power_down(void);
+
+  // wakes up the chip after power down mode
+  void power_up(void);
 };
-
-#endif /* HX711_2_h */
